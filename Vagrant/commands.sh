abaffe047b2f69f96f034a207ae7943160c09573
Imran Alam <112178589+alamimran613@users.noreply.github.com>
2024-01-03 18:48:50 +0530

    Create commands.sh

---
+++ Vagrant/commands.sh
@@ -0,0 +1,17 @@
+# Create VM
+vagrant init <box Name from vagrant cloud>
+
+# Power Up VM
+vagrant up
+
+# Power Down VM
+vagrant halt
+
+# Reload Updated Configurations
+vagrant reload
+
+# Destroy VM
+vagrant destroy
+
+# Login into VM
+vagrant ssh
