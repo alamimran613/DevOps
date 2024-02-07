9bed6edb9861dbb971bbf1133a85b3369c46cbad
alamimran613 <alamimran613@gmail.com>
2024-02-07 15:53:08 +0530

    add jenkins file

---
+++ Jenkins/jenkins-setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt update
+
+# Installing dependency of java, JDK version 11
+sudo apt install openjdk-11-jdk -y
+
+# Installing Maven
+sudo apt install maven -y
+
+# Installing Jenkins
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+  
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install jenkins -y
+###
